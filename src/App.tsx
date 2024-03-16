79653f4581ed6506be70350934f7f18f2ecde624
Andres Arana <and2arana@gmail.com>
2024-03-16 17:28:23 -0300

    Creates mock case list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function getCasesFromServer() {
+  return [
+    {
+      id: 1,
+      categoria: "Femicidio",
+      nombre_victima: "Agustina",
+      nombre_agresor: "Leandro",
+      ubicacion: "Santa Fe",
+      fecha: "2023-04-23T15:00:00.000Z"
+    },
+    {
+      id: 2,
+      categoria: "Intento de femicidio",
+      nombre_victima: "Nicole",
+      nombre_agresor: "Jeremias",
+      ubicacion: "Formosa",
+      fecha: "2022-11-11T16:23:45.000Z"
+    },
+    {
+      id: 3,
+      categoria: "Se investiga - Femicidio",
+      nombre_victima: "Victoria",
+      nombre_agresor: "Nicolas",
+      ubicacion: "Tierra del fuego",
+      fecha: "2020-09-23T23:30:30.000Z"
+    },
+    {
+      id: 4,
+      categoria: "Transfemicidio",
+      nombre_victima: "Naiara",
+      nombre_agresor: "Jose Luis",
+      ubicacion: "Mendoza",
+      fecha: "2024-01-23T23:30:30.000Z"
+    },
+    {
+      id: 5,
+      categoria: "Intento de Femicidio Vinculado",
+      nombre_victima: "Camila",
+      nombre_agresor: "Jorge",
+      ubicacion: "Buenos Aires",
+      fecha: "2017-06-11T23:30:30.000Z"
+    },
+  ]
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav className="navbar bg-body-tertiary navbar-expand-lg" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Registro de Femicidios</a>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope='col'>Categoría</th>
+              <th scope='col'>Víctima</th>
+              <th scope='col'>Agresor</th>
+              <th scope='col'>Ubicación</th>
+              <th scope='col'>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>
+            {getCasesFromServer().map((el)=>(
+              <tr key={el.id}>
+              <td>{el.categoria}</td>
+              <td>{el.nombre_victima}</td>
+              <td>{el.nombre_agresor}</td>
+              <td>{el.ubicacion}</td>
+              <td>{el.fecha}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
