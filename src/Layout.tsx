eb38f7a0a2a2499264a1368c2c06fbe6b20ea3f0
Agus-Coder <agus_code@outlook.com.ar>
2024-05-14 22:32:30 -0300

    Add custom first routes

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className="navbar bg-body-tertiary navbar-expand-lg" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Registro de Femicidios</a>
+      <div className="collapse navbar-collapse">
+        <Link className="navbar-brand" href="/">Registro de Femicidios</Link>
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <Link class="nav-link" to="/">Inicio</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/form">Form</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/cases">Casos</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
